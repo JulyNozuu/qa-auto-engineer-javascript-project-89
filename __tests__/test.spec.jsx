56621a63a412ec6ab1a05071655de1b415f0a869
Выставкина Юлия <yulya.reso@mail.ru>
2025-06-15 13:57:34 +0300

    Add test

--- __tests__/test.spec.jsx
+++ __tests__/test.spec.jsx
@@ -1,17 +1,23 @@
 import '@hexlet/chatbot-v2/styles'
-import {screen} from '@testing-library/dom'
 import Widget from '@hexlet/chatbot-v2';
 import steps from '@hexlet/chatbot-v2/example-steps';
 import '@hexlet/chatbot-v2/styles';
 import { test, expect } from 'vitest'
-import {render} from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { debug } from "vitest-preview";
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 test('renders a message', () => {
   const {getByRole} = render(Widget(steps))
   expect(getByRole('button', {name: 'Открыть Чат'})
   ).toBeVisible()
   screen.debug()
-  debug();
+})
+
+test('Button Open Chat', async() => {
+  const user = userEvent.setup()
+  render(Widget(steps))
+  await user.click(screen.getByRole('button', {name: 'Открыть Чат'}))
+  screen.debug()
 })
